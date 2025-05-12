a21dbd170adae65a09212a66c3f36c932bc6d940
Yajing1008 <yajing.libr@gmail.com>
2025-05-12 10:31:54 +0200

    first commit

---
+++ src/main/java/com/ut1/miage/appRS/AppRsApplication.java
@@ -0,0 +1,13 @@
+package com.ut1.miage.appRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ut1/miage/appRS/AppRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ut1.miage.appRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
