4d5d9846f256fdcd6384ae6dd725e09545dcbcb1
Yajing1008 <yajing.libr@gmail.com>
2025-05-14 11:53:47 +0200

    table Etudiant

--- src/main/java/com/ut1/miage/appRS/model/Etudiant.java
+++ src/main/java/com/ut1/miage/appRS/model/Etudiant.java
@@ -1,5 +1,85 @@
 package com.ut1.miage.appRS.model;
+import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ETUDIANTS")
 public class Etudiant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEtudiant;
+
+    private String nomEtudiant;
+    private String prenomEtudiant;
+
+    private LocalDate dateNaissanceEtudiant;
+
+    private String sexeEtudiant;
+
+    private String emailEtudiant;
+
+    private String motDePass;
+
+    public Long getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public void setIdEtudiant(Long idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+
+    public String getNomEtudiant() {
+        return nomEtudiant;
+    }
+
+    public void setNomEtudiant(String nomEtudiant) {
+        this.nomEtudiant = nomEtudiant;
+    }
+
+    public String getPrenomEtudiant() {
+        return prenomEtudiant;
+    }
+
+    public void setPrenomEtudiant(String prenomEtudiant) {
+        this.prenomEtudiant = prenomEtudiant;
+    }
+
+    public LocalDate getDateNaissanceEtudiant() {
+        return dateNaissanceEtudiant;
+    }
+
+    public void setDateNaissanceEtudiant(LocalDate dateNaissanceEtudiant) {
+        this.dateNaissanceEtudiant = dateNaissanceEtudiant;
+    }
+
+    public String getSexeEtudiant() {
+        return sexeEtudiant;
+    }
+
+    public void setSexeEtudiant(String sexeEtudiant) {
+        this.sexeEtudiant = sexeEtudiant;
+    }
+
+    public String getEmailEtudiant() {
+        return emailEtudiant;
+    }
+
+    public void setEmailEtudiant(String emailEtudiant) {
+        this.emailEtudiant = emailEtudiant;
+    }
+
+    public String getMotDePass() {
+        return motDePass;
+    }
+
+    public void setMotDePass(String motDePass) {
+        this.motDePass = motDePass;
+    }
     
 }
