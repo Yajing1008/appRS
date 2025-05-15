ae2a2b6206d8d0d8d7d8c21c7012dfc743c02a99
Yajing1008 <yajing.libr@gmail.com>
2025-05-15 09:50:59 +0200

    bootstrap

---
+++ src/main/java/com/ut1/miage/appRS/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.ut1.miage.appRS.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
