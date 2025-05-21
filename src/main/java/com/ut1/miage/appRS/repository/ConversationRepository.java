b19c2ca4c043f7f7a3178416e0b7f10b51b45af0
Yajing1008 <yajing.libr@gmail.com>
2025-05-21 19:25:47 +0200

    repo javadoc

--- src/main/java/com/ut1/miage/appRS/repository/ConversationRepository.java
+++ src/main/java/com/ut1/miage/appRS/repository/ConversationRepository.java
@@ -2,10 +2,12 @@ package com.ut1.miage.appRS.repository;
 
 import com.ut1.miage.appRS.model.Conversation;
 import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
+/**
+ * Interface de dépôt pour l'entité {@link Conversation}.
+ * Fournit des méthodes de base pour l'accès à la base de données via JPA.
+ * Étend {@link JpaRepository} pour bénéficier des opérations CRUD standard.
+ */
 public interface ConversationRepository extends JpaRepository<Conversation, Long> {
 
 }
